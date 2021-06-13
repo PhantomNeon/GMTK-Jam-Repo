b24120cc4809090d7ff985978a639d11bff34b24
PhantomNeon <guptaishaan06@gmail.com>
2021-06-13 15:09:49 +0530

    The beginning of the phantoms menace
    Deleted all the movement stuff and started redoing it using cinemachine

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,50 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Transform GroundCheck;
-    public PlayerJetPack playerJetPack;
-    public Transform playerCameraParent;
-    [Space]
-    private Vector2 rotation = Vector2.zero;
-    public float lookSpeed = 2.0f;
-    public float lookXLimit = 60.0f;
-    [Space]
-    public float NormalSpeed;
-    public float SprintSpeed;
-    private float MovementSpeed;
-    [Space]
-    public bool isGrounded = false;
-    public float GroundDistance;
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        rotation.y = transform.eulerAngles.y;
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance);
-
-        playerJetPack.CanMove = !isGrounded;
-
-
-
-        if (isGrounded)
-        {
-            MovementSpeed = Input.GetKey(KeyCode.LeftShift) ? SprintSpeed : NormalSpeed;
-            Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            transform.Translate(Movement * MovementSpeed * Time.deltaTime);
-        }
-
-        CameraMovement();
-    }
-
-    void CameraMovement()
-    {
-        rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
-        rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
-        rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
-        playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
-        transform.eulerAngles = new Vector2(0, rotation.y);
+        
     }
 }
