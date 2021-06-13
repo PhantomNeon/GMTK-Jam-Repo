28e9a324c071dc5070234afd96423f4012384eb0
PhantomNeon <guptaishaan06@gmail.com>
2021-06-13 13:34:14 +0530

    Fixing the movement
    Seprated jet pack and movement script, not done yet.

---
+++ Assets/Scripts/PlayerJetPack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJetPack : MonoBehaviour
+{
+
+
+    public float JetSpeed = 0.1f;
+    public float maxspeed = 1.5f;
+    private Vector3 moveDirection = Vector3.zero;
+    private Rigidbody rb;
+    public bool CanMove = true;
+
+    private void Start()
+    {
+          rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        float curSpeedX = CanMove ? JetSpeed * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = CanMove ? JetSpeed * Input.GetAxis("Horizontal") : 0;
+
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        
+        moveDirection = Vector3.ClampMagnitude(moveDirection, maxspeed);
+
+        if (CanMove)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                moveDirection.y = JetSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                moveDirection.y = -JetSpeed;
+            }
+        }
+
+        rb.AddForce(moveDirection, ForceMode.Impulse);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,87 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector2 rotation = Vector2.zero;
+    public Transform GroundCheck;
+    public PlayerJetPack playerJetPack;
     public Transform playerCameraParent;
+    [Space]
+    private Vector2 rotation = Vector2.zero;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 60.0f;
-    public float speed = 0.1f;
-    public float jumpSpeed = 1.0f;
-    public float maxspeed = 1.5f;
-    Vector3 moveDirection = Vector3.zero;
-    private Rigidbody rb;
-    public Vector3 Ground_check_position;
-    public bool Jetpack = false;
+    [Space]
+    public float NormalSpeed;
+    public float SprintSpeed;
+    private float MovementSpeed;
+    [Space]
+    public bool isGrounded = false;
+    public float GroundDistance;
 
-
-    [HideInInspector]
-    public bool canMove = true;
-    public bool grounded = true;
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-          rb = GetComponent<Rigidbody>();
-          rotation.y = transform.eulerAngles.y;
+        rotation.y = transform.eulerAngles.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        grounded = false;
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance);
 
-        float curSpeedX = canMove ? speed * Input.GetAxis("Vertical"):0;
-        float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal"):0;
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-        
-        moveDirection = Vector3.ClampMagnitude(moveDirection, maxspeed);
+        playerJetPack.CanMove = !isGrounded;
 
 
-        Collider[] colliders = Physics.OverlapBox(transform.position+Ground_check_position, Vector3.one / 2);
-        for (int i = 0; i < colliders.Length; i++)
+
+        if (isGrounded)
         {
-            if (colliders[i].gameObject != gameObject)
-            {
-
-                grounded = true;
-
-                if (!grounded)
-                    //first landing
-                    i = colliders.Length;
-            }
+            MovementSpeed = Input.GetKey(KeyCode.LeftShift) ? SprintSpeed : NormalSpeed;
+            Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            transform.Translate(Movement * MovementSpeed * Time.deltaTime);
         }
 
-        if (grounded)
-        {
-            if (Input.GetButton("Jump"))
-            {
-                rb.velocity = new Vector3(rb.velocity.x,jumpSpeed, rb.velocity.z);
-            }
-        }
-        else if (Jetpack)
-        {
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = speed;
-            }
+        CameraMovement();
+    }
 
-            if (Input.GetButton("Fire3"))
-            {
-                moveDirection.y = -speed;
-            }
-        }
-        
-        
-
-
-        rb.AddForce(moveDirection, ForceMode.Impulse);
-
-
-
-
+    void CameraMovement()
+    {
         rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
         rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
         rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
