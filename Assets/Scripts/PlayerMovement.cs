aa227f49feaa45b1696af08f2beb932bbec14cb4
PhantomNeon <guptaishaan06@gmail.com>
2021-06-13 15:33:56 +0530

    lesgo?
    Basic WASD movement in place, working on camera next.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
+    [Space]
+    public float NormalSpeed;
+    public float SprintSpeed;
+    private float MovementSpeed;
+    private Vector3 Movement = new Vector3();
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MovementSpeed = Input.GetKey(KeyCode.LeftShift) ? SprintSpeed : NormalSpeed;
+
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * MovementSpeed;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed;
+
+        Movement = transform.forward * vertical + transform.right * horizontal;
+
+        controller.Move(Movement);
     }
 }
